5e708ec95d53acbccdc02d31c5d254d72fb59755
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-13 08:40:09 +0600

    function create

--- js/diposit.js
+++ js/diposit.js
@@ -1,3 +1,22 @@
+
+
+function getInputFieldValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+function getElementValueByid(elementId){
+    const element =document.getElementById(elementId);
+    const elementValueString =element.innerText;
+    const value =parseFloat(elementValueString);
+    return value;
+}
+
 document.getElementById('btn-diposit').addEventListener('click',function(){
-    
+
+const newDipositAmmount = getInputFieldValueById('diposit-field');
+
 })
